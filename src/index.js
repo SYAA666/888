ddc26f64f399a11f0c2a3ee6a714db00c6805fc6
Kolja Glushakov <koooljaaan@gmail.com>
2018-03-15 16:16:23 +0300

    maybe fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  var bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257];
-  let divisor = 0; var counter=0;
-  for (let i = 0; bases[i] <= base; i++) {
-    if (base % bases[i] === 0) divisor = bases[i];
-  }
-  while(number > 0) {
-    number = Math.floor(number  / divisor);
-    counter += number;
-  }
-  return counter;
+    let prim = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257];
+    let maximalDevisor = 0
+    for (let i = 0; prim[i] <= base; i++) {
+        if (base % prim[i] === 0) {
+            maximalDevisor = prim[i]
+        }
+    }
+    let degr = 0, tmp = base
+    while (tmp % maximalDevisor === 0) {
+        degr++
+        tmp /= maximalDevisor
+    }
+    console.log(maximalDevisor)
+    let count = 0
+    while(number > 0) {
+        number = Math.floor(number  / maximalDevisor);
+        count += number;
+    }
+    return Math.floor(count / degr);
 }
